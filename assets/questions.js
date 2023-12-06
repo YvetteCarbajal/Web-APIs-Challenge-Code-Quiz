3eb9e4e29385ae9938e2d1545c4537328e15e027
YvetteCarbajal <134962298+YvetteCarbajal@users.noreply.github.com>
2023-12-06 08:02:27 -0600

    Add files via upload
    updated code

---
+++ assets/questions.js
@@ -0,0 +1,70 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Commonly used data types DO Not include:",
+        answer: "Booleans",
+        options: [
+            "String",
+            "Booleans",
+            "Alerts",
+            "Numbers",
+        ]
+    },
+    {
+        numb: 2,
+        question : "The condition of an if/else statement is enclosed within",
+        answer: "Curly Brackets",
+        options: [
+            "Quotes",
+            "Curly Brackets",
+            "Parentheses",
+            "Square Brackets",
+        ]
+    },
+    {
+        numb: 3,
+        question: "Arrays in Javascript can be used to store",
+        answer: "Booleans",
+        options: [
+            "Numbers and strings",
+            "Other Arrays",
+            "Booleans",
+            "All the Above"
+        ]
+    },
+    {
+        numb: 4,
+        question : "String values must be enclosed within _______ when being assigned to variables",
+        answer: "Quotes",
+        options: [
+            "Quotes",
+            "Curly Brackets",
+            "Commas",
+            "Parentheses",
+        ]
+    },
+    {
+        numb: 5,
+        question : "A very useful tool used during development and debugging for printing content to the debugger is: ",
+        answer: "Javascript",
+        options: [
+            "Javascript",
+            "Console.log",
+            "terminal/bash",
+            "For loops",
+        ]
+    },
+    {
+        numb: 6,
+        question : "Which of the following type of variable is visible only within a function where it is defined?",
+        answer: "local variable",
+        options: [
+            "Global Variable",
+            "Local Variable",
+            "Both of the above.",
+            "None of the above.",
+        ]
+    },
+ ];
+ 
+ 
